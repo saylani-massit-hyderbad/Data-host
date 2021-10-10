00b0546cd5aa8c288c6981d8b4042f2e19b1b00f
saylani-massit-hyderbad <saylanimasshyderabad@gmail.com>
2021-10-10 18:38:23 +0500

    new files

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { FcHome } from "react-icons/fc";
+import { useState } from "react";
+import { RiHome5Fill } from "react-icons/ri";
+
+import Button from '@material-ui/core/Button';
+
+import Card from
+
+function App(){
+
+  
+  return(
+    <>
+   <div>
+   <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+   <FcHome size='16'/></div> 
+  
+    <RiHome5Fill color='blue'/>
+    <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+    <h1>hello</h1>
+    </>
+  )
+}
+
+export default App;
